ad944dc87157c8c1364779fbecbfa283b37428c9
Anass <sebaianass@gmail.com>
2023-10-05 22:41:38 +0200

    gitignore

--- src/Ennemi/Ennemi.java
+++ src/Ennemi/Ennemi.java
@@ -9,6 +9,29 @@ import java.util.Random;
  */
 
 
+
+/*
+Une piste pour la création des ennemis:
+ * On peut créer les ennemis suivants:
+ * - Un gobelin: 10 points de vie, 3 points d'attaque, 1 point d'expérience
+ * - Un troll: 20 points de vie, 5 points d'attaque, 2 points d'expérience
+ * - Un dragon: 50 points de vie, 10 points d'attaque, 5 points d'expérience
+ * - Un loup: 15 points de vie, 4 points d'attaque, 1 point d'expérience
+ * - Un ours: 30 points de vie, 7 points d'attaque, 2 points d'expérience
+ * - Un serpent: 5 points de vie, 2 points d'attaque, 1 point d'expérience
+ * - Un squelette: 10 points de vie, 3 points d'attaque, 1 point d'expérience
+ * - Un zombie: 15 points de vie, 4 points d'attaque, 2 points d'expérience
+ * - Un fantôme: 5 points de vie, 2 points d'attaque, 1 point d'expérience
+ * - Un vampire: 20 points de vie, 5 points d'attaque, 3 points d'expérience
+ *  
+ * On peut aussi créer des boss:
+ * - Un boss gobelin: 20 points de vie, 6 points d'attaque, 2 points d'expérience
+ * - Un boss troll: 40 points de vie, 10 points d'attaque, 4 points d'expérience
+ * - Un boss dragon: 100 points de vie, 20 points d'attaque, 10 points d'expérience
+ * 
+ * 
+ */
+
 public class Ennemi {
     private String nom;
     private int pointsDeVie;
--- src/Jeu.java
+++ src/Jeu.java
@@ -2,7 +2,10 @@ import Personnage.*;
 public class Jeu {
     public static void main(String[] args) {
         Guerrier arion = new Guerrier();
+        Mage mage = new Mage();
         System.out.println(arion.getForce());
+        System.out.println(mage.getDescription());
+
         
     }
 }
